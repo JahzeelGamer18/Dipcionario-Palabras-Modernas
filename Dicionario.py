0584bd8775bf4f5b31f56b3366178e50df397786
JahzeelGamer18 <jahzeelgamer18@gmail.com>
2025-03-27 20:13:23 -0400

    Create Dicionario.py

---
+++ Dicionario.py
@@ -0,0 +1,17 @@
+import random
+
+meme_dict = {
+    "CRINGE": "Algo excepcionalmente raro o embarazoso",
+    "LOL": "Una respuesta común a algo gracioso",
+    "ROFL": "Una respuesta a una broma",
+    "SHEESH": "Ligera desaprobación",
+    "CREEPY": "Aterrador, siniestro",
+    "AGGRO": "Ponerse agresivo/enojado"
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict:
+    print({word}, {meme_dict[word]})
+else:
+    print("Lo siento, no conozco el significado de esta palabra.")
